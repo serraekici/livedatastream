90129b3ece2c4bf2c84a8bf18bdbc78d2ca35f26
serraekici <serra.ekici@hotmail.com>
2024-07-25 15:16:55 +0300

    15.16

--- data_gen.py
+++ data_gen.py
@@ -1,33 +1,35 @@
+
+import csv
 import random
-from itertools import count
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import time
 
-plt.style.use('fivethirtyeight')
+x_value = 0
+total_1 = 1000
+total_2 = 1000
 
-x_vals = []
-y_vals = []
-
-index = count()
+fieldnames = ["x_value", "total_1", "total_2"]
 
 
-def animate(i):
-    data = pd.read_csv('data.csv')
-    x = data['x_value']
-    y1 = data['total_1']
-    y2 = data['total_2']
+with open('data.csv', 'w') as csv_file:
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    csv_writer.writeheader()
 
-    plt.cla()
+while True:
 
-    plt.plot(x, y1, label='Channel 1')
-    plt.plot(x, y2, label='Channel 2')
+    with open('data.csv', 'a') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-    plt.legend(loc='upper left')
-    plt.tight_layout()
+        info = {
+            "x_value": x_value,
+            "total_1": total_1,
+            "total_2": total_2
+        }
 
+        csv_writer.writerow(info)
+        print(x_value, total_1, total_2)
 
-ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+        x_value += 1
+        total_1 = total_1 + random.randint(-6, 8)
+        total_2 = total_2 + random.randint(-5, 6)
 
-plt.tight_layout()
-plt.show()
+    time.sleep(1)
--- finished_file.py
+++ finished_file.py
@@ -1,25 +1,46 @@
-import csv
-import random
-import time
-x_value = 0
-total_1 = 1000
-total_2 = 1000
-fieldnames = ["x_value", "total_1", "total_2"]
-with open('data.csv', 'w') as csv_file: 
-    csv_writer = csv. DictWriter(csv_file, fieldnames=fieldnames) 
-    csv_writer.writeheader()
-while True:
-    with open('data.csv', 'a') as csv_file:
-        csv_writer = csv. DictWriter(csv_file, fieldnames=fieldnames)
+from itertools import count
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+plt.style.use('fivethirtyeight')
+
+x_vals = []
+y_vals = []
+
+plt.plot([], [], label='Channel 1')
+plt.plot([], [], label='Channel 2')
  
-    info = {
-"x_value": x_value,
-"total_1": total_1,
-"total_2": total_2
-}
-    csv_writer.writerow(info)
-    print(x_value, total_1, total_2)
-    x_value += 1
-    total_1 = total_1 + random.randint(-6, 8)
-    total_2 = total_2 + random.randint(-5, 6)
-    time.sleep (1)
+def animate(i):
+    data = pd.read_csv('data.csv')
+    x = data['x_value']
+    y1 = data['total_1']
+    y2 = data['total_2']
+
+    ax = plt.gca()
+    line1, line2 = ax.lines
+
+    line1.set_data(x, y1)
+    line2.set_data(x, y2)
+
+    xlim_low, xlim_high = ax.get_xlim()
+    ylim_low, ylim_high = ax.get_ylim()
+
+    ax.set_xlim(xlim_low, (x.max() + 5))
+
+    y1max = y1.max()
+    y2max = y2.max()
+    current_ymax = y1max if (y1max > y2max) else y2max
+
+    y1min = y1.min()
+    y2min = y2.min()
+    current_ymin = y1min if (y1min < y2min) else y2min
+
+    ax.set_ylim((current_ymin - 5), (current_ymax + 5))
+
+
+ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+
+plt.legend()
+plt.tight_layout()
+plt.show()
