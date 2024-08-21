7c671ee91eb879c7ffec330f1547712ece571cd2
Ersin OZTURK <ersnozturkk@gmail.com>
2024-08-21 17:56:34 +0300

    Created new branch with multiapp processes

---
+++ plotterfromserial/main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+def main():
+    black_screen = tk.Tk()
+    black_screen.configure(bg='black')
+    black_screen.geometry("400x400")
+    black_screen.title("Black Screen")
+    black_screen.mainloop()
+
+if __name__ == "__main__":
+    main()
